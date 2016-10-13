5d0771e632c8215274bf026d1ac7bf81731e9d4a
Jochen Greif <jochen@opoloo.de>
2016-10-13 15:30:38 +0200

    updated configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'middleman-template'
-set :repo_url, 'git@github.com:opoloo/middleman-template.git'
+set :application, 'manuskript'
+set :repo_url, 'git@github.com:opoloo/manuskript.git'
 set :scm, :middleman
 set :keep_releases, 2
